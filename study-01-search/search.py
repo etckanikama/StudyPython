55491284b7568481cdddb4d88715f8895e9436cc
etckanikama <funkybaby1230@ezweb.ne.jp>
2020-11-15 18:23:20 +0900

    課題1をコミット

---
+++ study-01-search/search.py
@@ -0,0 +1,37 @@
+### 検索ツールサンプル
+### これをベースに課題の内容を追記してください
+import csv
+# 検索ソース
+source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+
+### 検索ツール
+def search():
+    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+    sample = read_csv_hoge('data.csv')
+    for item in sample:
+        source.append(item)
+
+    ### ここに検索ロジックを書く
+    if word in source:
+      print("{}が見つかりした".format(word))
+    else:
+      print("{}はいません".format(word))
+      source.append(word)
+    print(source)
+
+
+
+
+
+def read_csv_hoge(data):
+    size=0
+    with open('data.csv') as f:
+        for line in csv.reader(f):
+            size = len(line)
+
+    return line
+
+        
+        
+if __name__ == "__main__":
+    search()
