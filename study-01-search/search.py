c7857d26963334928d55b8f1aeec572fdc6377c4
etckanikama <funkybaby1230@ezweb.ne.jp>
2020-11-15 19:51:05 +0900

    csvファイルをもとのデータとして定義

--- study-01-search/search.py
+++ study-01-search/search.py
@@ -1,34 +1,39 @@
-### 検索ツールサンプル
-### これをベースに課題の内容を追記してください
 import csv
+CSV_PATH = 'data.csv'
+
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
-
-### 検索ツール
-def search():
-    word =input("鬼滅の登場人物の名前を入力してください >>> ")
-    sample = read_csv_hoge('data.csv')
-    for item in sample:
-        source.append(item)
-
-    ### ここに検索ロジックを書く
-    if word in source:
-      print("{}が見つかりした".format(word))
-    else:
-      print("{}はいません".format(word))
-      source.append(word)
-    # print(source)
-
-
-
+# source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 # csvファイル読み込み
 def read_csv_hoge(data):
-    size=0
-    with open('data.csv') as f:
+    with open(CSV_PATH) as f:
         for line in csv.reader(f):
-            size = len(line)
+            pass
     return line
+
+# def write_csv(data,word):
+#     with open(CSV_PATH,'a') as f:
+#         writer = csv.writer(f)
+#         writer.writerow([','+ word])
+#     return writer
+
+## 検索ツール
+def search():
+    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+
+    array = read_csv_hoge(CSV_PATH)
+    ### ここに検索ロジックを書く
+    if word in array:
+        print("{}が見つかりした".format(word))
+    else:
+        print("{}はいません".format(word))
+        # w = write_csv(CSV_PATH, word)
+        # with open(CSV_PATH,'a') as f:
+        #     writer = csv.writer(f, lineterminator=',')
+        #     writer.writerow([word])
+    # a = read_csv_hoge(CSV_PATH)
+    # print(a)
+
 
         
         
