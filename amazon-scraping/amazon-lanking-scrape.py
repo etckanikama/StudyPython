82b50230d18d2356ca59835e336ff740f54494a3
etckanikama <funkybaby1230@ezweb.ne.jp>
2020-12-04 04:22:59 +0900

    amazon-lankingにて

--- amazon-scraping/amazon-lanking-scrape.py
+++ amazon-scraping/amazon-lanking-scrape.py
@@ -6,6 +6,16 @@ import datetime
 import pandas as pd
 import sys
 
+# データ格納配列
+custom_name_list = []
+custom_url_list = []
+detail_name = []
+detail_price = []
+detail_deadline = []
+detail_ansi = []
+
+
+
 def set_driver(driver_path, headless_flg):
     # Chromeドライバーの読み込み
     options = ChromeOptions()
@@ -27,10 +37,8 @@ def set_driver(driver_path, headless_flg):
 
 
 def main():
-    # データ格納配列
-    custom_name_list = []
-    custom_url_list = []
 
+    FIST_URL = "https://www.amazon.co.jp/gp/bestsellers/kitchen/ref=zg_bs_kitchen_home_all?"
     # driverを起動
     if os.name == 'nt': #Windows
         driver = set_driver("chromedriver.exe", False)
@@ -38,33 +46,34 @@ def main():
         driver = set_driver("chromedriver", False)
 
         # Webサイトを開く
-    driver.get("https://www.amazon.co.jp/gp/bestsellers/kitchen/ref=zg_bs_kitchen_home_all?")
+    driver.get(FIST_URL)
     time.sleep(5)
 
     while (True):
-
-        # 商品URLを取得
-        # url_xpath = driver.find_elements_by_xpath("//div[@class='a-row']/a[@class='a-link-normal a-text-normal']")
-        # for i in range(len(url_xpath)):
-        #     custom_url_list.append(url_xpath[i].get_attribute("href"))
-        #     # print(url_xpath[i].get_attribute("href"))
-        # print(len(custom_url_list))
-
-        url_xpath = driver.find_elements_by_xpath("//span[@class='aok-inline-block zg-item']/a[@class='a-link-normal']")
-        for i in range(len(url_xpath)):
-            custom_url_list.append(url_xpath[i].get_attribute("href"))
-            # print(url_xpath[i].get_attribute("href"))
-        print(len(custom_url_list))
         
 
 
-
+        # 商品名
         custom_name_get = driver.find_elements_by_xpath("//div[@class='p13n-sc-truncate-desktop-type2 p13n-sc-truncated']")
-        #　商品名を取得してくる
         for name in custom_name_get:
             custom_name_list.append(name.text)
             # print(name.text)
         print(len(custom_name_list))
+        
+        # 商品url
+        url_xpath = driver.find_elements_by_xpath("//span[@class='aok-inline-block zg-item']/a[@class='a-link-normal']")
+        for i in range(len(url_xpath)):
+            detail_url = url_xpath[i].get_attribute("href")
+            detail_info(driver,detail_url)
+            time.sleep(5)
+            # これだけ
+            driver.implicitly_wait(10)
+            # 仮説：以下で再度urlを更新した際にforの外でとったurl_xpathの情報がなかったことになってしまうため
+            driver.get(FIST_URL)
+            time.sleep(10)
+            custom_url_list.append(detail_url)
+            # print(url_xpath[i].get_attribute("href"))
+        # print(len(custom_url_list))
 
 
 
@@ -79,10 +88,49 @@ def main():
         except:
             print("商品ページはこれ以上存在しません")
             break
-    colum = ['商品名','商品url']
+
+
+    colum = ['商品名','商品url','価格','ANSI番号']
     # df作成
-    df = pd.DataFrame(list(zip(custom_name_list,custom_url_list)),columns=colum)
+    df = pd.DataFrame(list(zip(custom_name_list,custom_url_list,detail_price,detail_ansi)),columns=colum)
     df.to_csv("custom_info.csv",encoding="utf-8_sig")
+    
+#　詳細画面 
+def detail_info(driver,detail_url):
+
+        # ☆おそらくここgetがメソッドとして認識されていない
+        driver.get(f'{detail_url}')
+        time.sleep(5)
+        title_name = driver.find_element_by_xpath("//span[@class='a-size-large product-title-word-break']")
+        # for name in title_name:
+        print("商品名：{}".format(title_name.text))
+        detail_name.append(title_name.text)
+        
+        price = driver.find_element_by_xpath("//span[@class='a-size-medium a-color-price priceBlockBuyingPriceString']") 
+        print("価格：{}".format(price.text))
+        detail_price.append(price.text)
+        
+        deadline = driver.find_element_by_xpath("//div[@class='a-section a-spacing-mini']")
+        print(deadline.text)
+        detail_deadline.append(deadline.text)
+
+        # primeかどうか
+        # 複数個あるときはelementsのsを忘れてはいけない
+        prime_check = driver.find_elements_by_xpath("//div[@class='nav-line-1-container']")[1]
+
+        if prime_check.text != "今すぐ登録":
+            # prime_label = True
+            print("prime会員です。")
+        print("prime会員ではありません")
+
+        cur_url = driver.current_url
+        front = cur_url.find("/dp")
+        back =cur_url.find("/ref")
+        print("ASIN番号：{}".format(cur_url[front+4:back]))
+        detail_ansi.append(cur_url[front+4:back])
+        
+        time.sleep(5)
+
 
 
 
